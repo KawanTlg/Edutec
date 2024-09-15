419cb4f10fb8af0d6992a91047da083ae2e6d644
Kawan Ribeiro <kawanribeiro.ph2@gmail.com>
2024-09-15 00:06:46 +0000

    arrumação na barra de cima

---
+++ quiz.js
@@ -0,0 +1,103 @@
+const perguntas = [
+    //quiz1//
+    {
+     pergunta: "Quem criou o primeiro modelo atomico?",
+     respostas:[
+        {texto: "Thomson",correto:false},
+        {texto: "Rutherford",correto:false},
+        {texto: "Bohr",correto:false},
+        {texto: "Dalton", correto:true}
+    ]
+},
+    //quiz2//
+{
+    pergunta: "Quem organizou a Tabela Periódica?",
+    respostas:[
+       {texto: "Dmitri Mendeleev",correto:true},
+       {texto: "Albert Einstein",correto:false},
+       {texto: "Marie Curie",correto:false},
+       {texto: "NASA", correto:false}
+   ]
+},
+    //quiz3//
+{
+    pergunta: "Qual modelo atômico é conhecido como Pudim de Passas?",
+    respostas:[
+       {texto: "Modelo de Daton",correto:false},
+       {texto: "Modelo de Bohr",correto:false},
+       {texto: "Modelo de Thomson",correto:true},
+       {texto: "Modelo de Rutherford", correto:false}
+   ]
+},
+    //quiz4//
+{
+    pergunta: "Quem criou o primeiro modelo atomico?",
+    respostas:[
+       {texto: "Thomson",correto:false},
+       {texto: "Rutherford",correto:false},
+       {texto: "Bohr",correto:false},
+       {texto: "Dalton", correto:true}
+   ]
+},
+    //quiz5//
+{
+    pergunta: "Quem criou o primeiro modelo atomico?",
+    respostas:[
+       {texto: "Thomson",correto:false},
+       {texto: "Rutherford",correto:true},
+       {texto: "Bohr",correto:false},
+       {texto: "Dalton", correto:false}
+   ]
+},
+    //quiz6//
+{
+    pergunta: "Quem criou o primeiro modelo atomico?",
+    respostas:[
+       {texto: "Thomson",correto:true},
+       {texto: "Rutherford",correto:false},
+       {texto: "Bohr",correto:false},
+       {texto: "Dalton", correto:false}
+   ]
+},
+    //quiz7//
+{
+    pergunta: "Quem criou o primeiro modelo atomico?",
+    respostas:[
+       {texto: "Thomson",correto:false},
+       {texto: "Rutherford",correto:true},
+       {texto: "Bohr",correto:false},
+       {texto: "Dalton", correto:false}
+   ]
+},
+    //quiz8//
+{
+    pergunta: "Quem criou o primeiro modelo atomico?",
+    respostas:[
+       {texto: "Thomson",correto:false},
+       {texto: "Rutherford",correto:true},
+       {texto: "Bohr",correto:false},
+       {texto: "Dalton", correto:false}
+   ]
+},
+    //quiz9//
+{
+    pergunta: "Quem criou o primeiro modelo atomico?",
+    respostas:[
+       {texto: "Thomson",correto:false},
+       {texto: "Rutherford",correto:false},
+       {texto: "Bohr",correto:true},
+       {texto: "Dalton", correto:false}
+   ]
+},
+    //quiz10//
+{
+    pergunta: "Quem criou o primeiro modelo atomico?",
+    respostas:[
+       {texto: "Thomson",correto:false},
+       {texto: "Rutherford",correto:false},
+       {texto: "Bohr",correto:false},
+       {texto: "Dalton", correto:true}
+   ]
+}
+
+]
